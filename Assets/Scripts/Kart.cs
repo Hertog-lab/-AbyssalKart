754360eaf5daa2561b8cbdddd41229599b520dce
chirp777 <supersonicsparky7@gmail.com>
2022-11-16 12:03:42 +0100

    Added a freeze function

--- Assets/Scripts/Kart.cs
+++ Assets/Scripts/Kart.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Kart : MonoBehaviour
 {
+    public bool freeze = false;
     public Vector2Int input = new Vector2Int();
     
     [Header("Linear movement")]
@@ -30,71 +31,73 @@ public class Kart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        input = new Vector2Int(
-        ((Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) ? 0 : 
-            ((Input.GetKey(KeyCode.A)) ? -1 : 
-            ((Input.GetKey(KeyCode.D)) ? 1 : 0))), 
-        //If both are pressed, do nothing. If left is held, input.x is -1, if right is held input.x is 1.
-        ((Input.GetKey(KeyCode.W)) ? 1 : 0));
-        //If up is held, input.y is 1. No brakes :)
-        
-        //Accelerate towards target acceleration.
-        acceleration = Mathf.Clamp(Mathf.MoveTowards(acceleration, acceleration+passiveAcceleration, (Time.deltaTime*passiveAcceleration) * ((passiveAccelerate ? 1 : 0)+input.y)), -targetAcceleration, targetAcceleration);
-        accelerationBoost = Mathf.Clamp(Mathf.MoveTowards(accelerationBoost, 0, Time.deltaTime*passiveAcceleration*2), 0, Mathf.Infinity);
-        
-        //Apply drifting
-        if (Input.GetKey(KeyCode.Space))
-        {
-            if (drifting != true)
+        if (!freeze) {
+            input = new Vector2Int(
+            ((Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.D)) ? 0 : 
+                ((Input.GetKey(KeyCode.A)) ? -1 : 
+                ((Input.GetKey(KeyCode.D)) ? 1 : 0))), 
+            //If both are pressed, do nothing. If left is held, input.x is -1, if right is held input.x is 1.
+            ((Input.GetKey(KeyCode.W)) ? 1 : 0));
+            //If up is held, input.y is 1. No brakes :)
+
+            //Accelerate towards target acceleration.
+            acceleration = Mathf.Clamp(Mathf.MoveTowards(acceleration, acceleration+passiveAcceleration, (Time.deltaTime*passiveAcceleration) * ((passiveAccelerate ? 1 : 0)+input.y)), -targetAcceleration, targetAcceleration);
+            accelerationBoost = Mathf.Clamp(Mathf.MoveTowards(accelerationBoost, 0, Time.deltaTime*passiveAcceleration*2), 0, Mathf.Infinity);
+
+            //Apply drifting
+            if (Input.GetKey(KeyCode.Space))
             {
-                if (steerDir > (steerBounds/2))
+                if (drifting != true)
                 {
-                    driftDirection = -1;
-                    drifting = true;
+                    if (steerDir > (steerBounds/2))
+                    {
+                        driftDirection = -1;
+                        drifting = true;
+                    }
+                    if (steerDir < -(steerBounds/2))
+                    {
+                        driftDirection = 1;
+                        drifting = true;
+                    }
                 }
-                if (steerDir < -(steerBounds/2))
+                else
                 {
-                    driftDirection = 1;
-                    drifting = true;
+                    driftPower += Time.deltaTime*2;
                 }
             }
             else
             {
-                driftPower += Time.deltaTime*2;
+                if (drifting == true)
+                {
+                    accelerationBoost += Mathf.Clamp(driftPower, 0, targetAcceleration*2);
+                    driftDirection = 0;
+                    driftPower = 0;
+                    drifting = false;
+                }
             }
-        }
-        else
-        {
-            if (drifting == true)
+
+            //Steering (clamped between steerBounds)
+            if (drifting)
             {
-                accelerationBoost += Mathf.Clamp(driftPower, 0, targetAcceleration*2);
-                driftDirection = 0;
-                driftPower = 0;
-                drifting = false;
+                if (driftDirection == -1) {   
+                    //Drifting to the left
+                    steerDir = Mathf.Clamp(Mathf.MoveTowards(steerDir, (steerBounds*(input.x+1.5f))/1.5f, Time.deltaTime*(smoothSteer*10)), -steerBounds*2*(acceleration/targetAcceleration), steerBounds*2*(acceleration/targetAcceleration));
+                }
+                else if (driftDirection == 1) {
+                    //Drifting to the right
+                    steerDir = Mathf.Clamp(Mathf.MoveTowards(steerDir, (steerBounds*(input.x-1.5f))/1.5f, Time.deltaTime*(smoothSteer*10)), -steerBounds*2*(acceleration/targetAcceleration), steerBounds*2*(acceleration/targetAcceleration));
+                }
+                else {drifting = false;}
             }
-        }
-        
-        //Steering (clamped between steerBounds)
-        if (drifting)
-        {
-            if (driftDirection == -1) {   
-                //Drifting to the left
-                steerDir = Mathf.Clamp(Mathf.MoveTowards(steerDir, (steerBounds*(input.x+1.5f))/1.5f, Time.deltaTime*(smoothSteer*10)), -steerBounds*2*(acceleration/targetAcceleration), steerBounds*2*(acceleration/targetAcceleration));
+            else {
+
+                //Normal steering
+                steerDir = Mathf.Clamp(Mathf.MoveTowards(steerDir, steerBounds*input.x, Time.deltaTime*(smoothSteer*10)), -steerBounds*(acceleration/targetAcceleration), steerBounds*(acceleration/targetAcceleration));
             }
-            else if (driftDirection == 1) {
-                //Drifting to the right
-                steerDir = Mathf.Clamp(Mathf.MoveTowards(steerDir, (steerBounds*(input.x-1.5f))/1.5f, Time.deltaTime*(smoothSteer*10)), -steerBounds*2*(acceleration/targetAcceleration), steerBounds*2*(acceleration/targetAcceleration));
-            }
-            else {drifting = false;}
+
+            //Apply acceleration & steering
+            transform.position += transform.forward * (acceleration+accelerationBoost) * Time.deltaTime;
+            transform.Rotate(0, Time.deltaTime*(steerDir*10) ,0);
         }
-        else {
-        
-            //Normal steering
-            steerDir = Mathf.Clamp(Mathf.MoveTowards(steerDir, steerBounds*input.x, Time.deltaTime*(smoothSteer*10)), -steerBounds*(acceleration/targetAcceleration), steerBounds*(acceleration/targetAcceleration));
-        }
-        
-        //Apply acceleration & steering
-        transform.position += transform.forward * (acceleration+accelerationBoost) * Time.deltaTime;
-        transform.Rotate(0, Time.deltaTime*(steerDir*10) ,0);
     }
 }
