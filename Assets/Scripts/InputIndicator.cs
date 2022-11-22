1e6e4a4a9e09100b6bbcc21dca534ff971eae304
sem <78421702+Sem-Glu@users.noreply.github.com>
2022-11-22 14:31:22 +0100

    Input Indicator

---
+++ Assets/Scripts/InputIndicator.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputIndicator : MonoBehaviour
+{
+    [SerializeField] private GameObject UpButton;
+    [SerializeField] private GameObject DownButton;
+    [SerializeField] private GameObject LeftButton;
+    [SerializeField] private GameObject RightButton;
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            UpButton.SetActive(true);
+        }
+        else
+        {
+            UpButton.SetActive(false);
+        }
+        
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+        {
+            DownButton.SetActive(true);
+        }
+        else
+        {
+            DownButton.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            LeftButton.SetActive(true);
+        }
+        else
+        {
+            LeftButton.SetActive(false);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            RightButton.SetActive(true);
+        }
+        else
+        {
+            RightButton.SetActive(false);
+        }
+    }
+}
