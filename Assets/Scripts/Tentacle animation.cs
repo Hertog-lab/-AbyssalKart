41e3cc435b3b10d076b2ddac7f6e5a5b8d5c0dc0
sem <78421702+Sem-Glu@users.noreply.github.com>
2022-11-29 10:48:29 +0100

    idk main scenes's update

---
+++ Assets/Scripts/Tentacle animation.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacleanimation : MonoBehaviour
+{
+    [SerializeField] private GameObject Base;
+    [SerializeField] private GameObject Mid;
+    [SerializeField] private GameObject Top;
+
+
+}
