2b981d8879295f841b7d6eec3bea014877c826d5
chirp777 <supersonicsparky7@gmail.com>
2022-11-30 15:30:50 +0100

    camera can now predict movement
    (toggleable with boolean)

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    public float speed = 8;
+    [SerializeField] private float speed = 8;
     public Transform target;
+    public bool anticipateMovement;
+    [SerializeField] private float anticipateAmount;
+    
+    private Vector3 prevPos, curPos;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +20,19 @@ public class CameraFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime*speed);
+        
+        if (anticipateMovement)
+        {
+            curPos = target.position;
+            Vector3 diff = (curPos - prevPos)/Time.deltaTime*anticipateAmount;
+            
+            transform.position = Vector3.Lerp(transform.position, target.position + diff, Time.deltaTime*speed);
+            
+            prevPos = curPos;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime*speed);
+        }
     }
 }
