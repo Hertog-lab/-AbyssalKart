5a0602303e6f86503467972be30d5b5e9a28edf5
sem <78421702+Sem-Glu@users.noreply.github.com>
2022-12-20 13:35:23 +0100

    place obstacle thingy

---
+++ Assets/Scripts/Obstacle generation/PlaceObjects.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceObjects : MonoBehaviour
+{
+    [SerializeField] private GameObject TentaclePre;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Period)) 
+    }
+
+    private void PlaceObject()
+    {
+
+    }
+}
