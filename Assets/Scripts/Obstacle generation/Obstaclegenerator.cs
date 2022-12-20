f329fed3d98252cffe6926a8e47d60694400a872
sem <78421702+Sem-Glu@users.noreply.github.com>
2022-12-20 11:14:51 +0100

    obstacle generations and other things

--- Assets/Scripts/Obstacle generation/Obstaclegenerator.cs
+++ Assets/Scripts/Obstacle generation/Obstaclegenerator.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Obstaclegenerator : MonoBehaviour
@@ -7,38 +5,19 @@ public class Obstaclegenerator : MonoBehaviour
     [SerializeField] private GameObject[] TentacleLocations;
     [SerializeField] private GameObject[] MouthLocations;
 
-
-    private void Update()
-    {
-        
-    }
-
     private void Start()
     {
-        foreach (var Tentacle in TentacleLocations)
-        {
-            Tentacle.SetActive(false);
-        }
-        
-        foreach (var Mouth in MouthLocations)
-        {
-            Mouth.SetActive(false);
-        }
-
-        int random;
-        random = Random.Range(0, TentacleLocations.Length);
-        SpawnObstacle(random, true);
+        foreach (var Tentacle in TentacleLocations) Tentacle.SetActive(false);
+        foreach (var Mouth in MouthLocations) Mouth.SetActive(false);           
     }
-
-    private void SpawnObstacle(int obstacleNumber, bool IsTentacle)
+    /// <summary>
+    /// activate an obstacle
+    /// </summary>
+    /// <param name="obstacleNumber">number of obstalce in array</param>
+    /// <param name="Obstacle_ID">ID of the type of obstacle</param>
+    public void SpawnObstacle(int obstacleNumber, int Obstacle_ID)
     {
-        if (IsTentacle)
-        {
-            TentacleLocations[obstacleNumber].SetActive(true);
-        }
-        else
-        {
-            MouthLocations[obstacleNumber].SetActive(true);
-        }
+        if (Obstacle_ID == 0) TentacleLocations[obstacleNumber].SetActive(true);
+        else if(Obstacle_ID == 1) MouthLocations[obstacleNumber].SetActive(true);
     }
 }
